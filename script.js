5537ed1982d17907041051d57d816f1e419b0ffc
Adam <adammiller.2000nj@gmail.com>
2023-03-30 15:24:25 -0400

    reformatted js/added calculator constructor

--- script.js
+++ script.js
@@ -1,37 +1,14 @@
+class Calculator {
+    constructor(screenCurrent, screenPrevious) {
+        this.screenPrevious = this.screenPrevious;
+        this.screenCurrent = this.screenCurrent;
+    }
+}
+
 const numKeys = document.querySelectorAll('.num');
 const opKeys = document.querySelectorAll('.op');
-let num1 = 0;
-let num2 = 0;
-let result = 0;
-
-function add(num1, num2) {
-    
-}
-
-function subtract(num1, num2) {
-    
-}
-
-function multiply(num1, num2) {
-    
-}
-
-function divide(num1, num2) {
-    
-}
-
-function modulus(num1, num2) {
-    
-}
-
-function clear(num1, num2) {
-    
-}
-
-function allClear(num1, num2) {
-    
-}
-
-function equals(num1, num2) {
-    
-}
+const equal = document.getElementById("equalBtn");
+const clear = document.getElementById("clearBtn");
+const allClear = document.getElementById("allClearBtn");
+const screenCurrent = document.getElementById('currentOperator');
+const screenPrevious = document.getElementById('previousOperator');
